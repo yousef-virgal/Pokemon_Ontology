ebcaa906609279e2660512808b844a2e8196c8ba
Yousef Shafik <yousef.shafik@techsa.io>
2023-05-19 11:39:57 +0300

    -[Develpoment] Adds a text area in the frontend to query, adds route /query in the backend and fetches the queried string when a button is clicked

--- Server/app.py
+++ Server/app.py
@@ -1,9 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, make_response
+from rdflib import Graph
+
+import Constants
+
 app = Flask(__name__)
 
+graph = Graph()
+graph.parse("./Ontology/ontology.xml",format="xml")
+
 @app.route("/")
-def hello():
+def index():
   return render_template('index.html', message = "hello world")
 
+@app.route("/query",methods=['GET'])
+def query():
+  queryString = request.args.get("q")
+  if queryString == None:
+    return make_response(jsonify({"error":"Invalid query parameter", "status": Constants.BAD}), Constants.BAD)
+
+  try :
+
+    response = graph.query(queryString)
+
+    data = []
+    for row in response:
+      if isinstance(row, bool):
+        return make_response(jsonify({"response": row, "status": Constants.OK}), Constants.OK)
+      
+      elementArray = [] 
+      for element in row:
+        elementArray.append(element)
+      data.append(elementArray)
+
+    return make_response(jsonify({"response": data, "status": Constants.OK}), Constants.OK) 
+
+  except Exception as e:
+    print(e)
+    return make_response(jsonify({"error": "Invalid Query", "status": Constants.BAD}), Constants.BAD) 
+  
+
 if __name__ == "__main__":
+  
   app.run(debug=True)
+  
--- Server/static/index.js
+++ Server/static/index.js
@@ -1,4 +1,21 @@
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
+  const buttons = document.querySelectorAll("button");
 
-    console.log(document.getElementsByTagName("h1"))
-})
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const textArea = document.querySelector("textarea");
+      console.log(textArea.value)
+      fetch(
+        "/query?" +
+          new URLSearchParams({
+            q: textArea.value,
+          })
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => console.log(data))
+        .catch((error) => console.log(error));
+    });
+  });
+});
