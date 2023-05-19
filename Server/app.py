66fc813047c6bc7d077e66a0be7ad7d2ec883e66
Yousef Shafik <yousef.shafik@techsa.io>
2023-05-20 02:59:16 +0300

    Revert "Merge branch 'frontend'"
    This reverts commit 1623b641c61c69645f9e3c62cb968959e302a5a7, reversing
    changes made to ca584341b42fb2053e22f9edd19a674e01187c77.

---
+++ Server/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, jsonify, request, make_response
+from flask_cors import CORS
+from rdflib import Graph
+
+import Constants
+
+app = Flask(__name__)
+CORS(app)
+
+graph = Graph()
+graph.parse("./Ontology/ontology.xml",format="xml")
+
+@app.route("/")
+def index():
+  return render_template('index.html', message = "hello world")
+
+@app.route("/query",methods=['GET'])
+def query():
+  queryString = request.args.get("q")
+  if queryString == None:
+    return make_response(jsonify({"error":"Invalid query parameter", "status": Constants.BAD}), Constants.BAD)
+
+  try :
+
+    response = graph.query(queryString)
+
+    data = []
+    for row in response:
+      if isinstance(row, bool):
+        return make_response(jsonify({"response": row, "status": Constants.OK}), Constants.OK)
+      
+      elementArray = [] 
+      for element in row:
+        elementArray.append(element)
+      data.append(elementArray)
+
+    return make_response(jsonify({"response": data, "status": Constants.OK}), Constants.OK) 
+
+  except Exception as e:
+    print(e)
+    return make_response(jsonify({"error": "Invalid Query", "status": Constants.BAD}), Constants.BAD) 
+  
+
+if __name__ == "__main__":
+  
+  app.run(debug=True)
+  
